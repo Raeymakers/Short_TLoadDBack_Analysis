7feabf65f5ee7486eab523cca94943e87e423fb0
Raeymakers <sofie.raeymakers@outlook.com>
2022-05-03 11:18:24 +0200

    some more preprocessing

--- preprocessing.R
+++ preprocessing.R
@@ -45,75 +45,84 @@ combined_PVT <- lapply(files, read.csv) %>%
 
 # exclude rows that contain END OF FILE 
 combined_PVT<-combined_PVT[combined_PVT$ï..Event.Index!="END OF FILE",]
+combined_PVT<-combined_PVT[combined_PVT$Trial.Number!="BEGIN TASK",]
+
+#remove instructions
+combined_PVT<-combined_PVT[combined_PVT$display!="instructions",]
+
 
 #export combined data as a CSV. 
 write.csv(combined_PVT,"combined_PVT.csv",row.names=FALSE)
 
 #re-download
-PVT <- read.csv(paste0(Dir, "combined_PVT.csv"), header = TRUE, sep = )
+combined_PVT <- read.csv(paste0(Dir, "combined_PVT.csv"), header = TRUE, sep = )
 
 
 # make columns for conditions: time = 1 or 2, task= PVT1, PVT2, etc
 
-PVT$Day[PVT$Tree.Node.Key == "task-1lhj"] = 1 # was taken on day 1
-PVT$Test[PVT$Tree.Node.Key == "task-1lhj"] = 1 # before TloadDback
-PVT$Condition[PVT$Tree.Node.Key == "task-1lhj"] = 'LCL' # Acc= 1
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-1lhj"] = 1 # was taken on day 1
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-1lhj"] = 1 # before TloadDback
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-1lhj"] = 'LCL' # Acc= 1
 
-PVT$Day[PVT$Tree.Node.Key == "task-ofgl"] = 1
-PVT$Test[PVT$Tree.Node.Key == "task-ofgl"] = 2
-PVT$Condition[PVT$Tree.Node.Key == "task-ofgl"] = 'LCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-ofgl"] = 1
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-ofgl"] = 2
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-ofgl"] = 'LCL'
 
-PVT$Day[PVT$Tree.Node.Key == "task-3l6b"] = 2
-PVT$Test[PVT$Tree.Node.Key == "task-3l6b"] = 1
-PVT$Condition[PVT$Tree.Node.Key == "task-3l6b"] = 'LCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-3l6b"] = 2
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-3l6b"] = 1
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-3l6b"] = 'LCL'
 
-PVT$Day[PVT$Tree.Node.Key == "task-6xhv"] = 2
-PVT$Test[PVT$Tree.Node.Key == "task-6xhv"] = 2
-PVT$Condition[PVT$Tree.Node.Key == "task-6xhv"] = 'LCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-6xhv"] = 2
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-6xhv"] = 2
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-6xhv"] = 'LCL'
 
 
-PVT$Day[PVT$Tree.Node.Key == "task-jmi7"] = 1
-PVT$Test[PVT$Tree.Node.Key == "task-jmi7"] = 1
-PVT$Condition[PVT$Tree.Node.Key == "task-jmi7"] = 'HCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-jmi7"] = 1
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-jmi7"] = 1
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-jmi7"] = 'HCL'
 
-PVT$Day[PVT$Tree.Node.Key == "task-82mr"] = 1
-PVT$Test[PVT$Tree.Node.Key == "task-82mr"] = 2
-PVT$Condition[PVT$Tree.Node.Key == "task-82mr"] = 'HCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-82mr"] = 1
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-82mr"] = 2
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-82mr"] = 'HCL'
 
-PVT$Day[PVT$Tree.Node.Key == "task-dt2v"] = 2
-PVT$Test[PVT$Tree.Node.Key == "task-dt2v"] = 1
-PVT$Condition[PVT$Tree.Node.Key == "task-dt2v"] = 'HCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-dt2v"] = 2
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-dt2v"] = 1
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-dt2v"] = 'HCL'
 
-PVT$Day[PVT$Tree.Node.Key == "task-5too"] = 2
-PVT$Test[PVT$Tree.Node.Key == "task-5too"] = 2
-PVT$Condition[PVT$Tree.Node.Key == "task-5too"] = 'HCL'
+combined_PVT$Day[combined_PVT$Tree.Node.Key == "task-5too"] = 2
+combined_PVT$Test[combined_PVT$Tree.Node.Key == "task-5too"] = 2
+combined_PVT$Condition[combined_PVT$Tree.Node.Key == "task-5too"] = 'HCL'
 
-PVT$Day <- factor(PVT$Day)
-PVT$Test <- factor(PVT$Test)
-PVT$Condition <- factor(PVT$Condition)
+combined_PVT$Day <- factor(combined_PVT$Day)
+combined_PVT$Test <- factor(combined_PVT$Test)
+combined_PVT$Condition <- factor(combined_PVT$Condition)
 
-levels(PVT$Condition)
+levels(combined_PVT$Condition)
 
 #create sequential IDs
-levs<-unique(PVT$Participant.Private.ID)
-PVT$ID <- factor(PVT$Participant.Private.ID, levels=levs, labels=seq_along(levs))
+levs<-unique(combined_PVT$Participant.Private.ID)
+combined_PVT$ID <- factor(combined_PVT$Participant.Private.ID, levels=levs, labels=seq_along(levs))
+
+#rename column
+combined_PVT %>% 
+  rename(randomiser.2vg5 = Accuracy_Level)
+
 
 #round RT
-PVT$RT = round(PVT$Reaction.Time)
+combined_PVT$RT = round(combined_PVT$Reaction.Time)
 
-#check timezone: if Local.Timezone != 1, remove!
+#people that used mobile instead of computer
+combined_PVT$ID[combined_PVT$Participant.Device.Type =="mobile"]
 
-#Check Participant Status
-
-#check that participant.Device.Type == computer
 
 #check Participant.Browser and Participant.OS and Participant.Monitor.size
+unique(combined_PVT$Participant.Browser)
 
 #Drop unnecessary columns
-PVT = subset(PVT, select = -c(UTC.Timestamp,Local.Timestamp,Experiment.ID,Repeat.Key, Schedule.ID, Participant.Public.ID, Participant.Starting.Group, Participant.Completeion.Code,
-                              Participant.External.Session.ID, Participant.Device.Type, Participant.Device, Participant.Browser, Participant.Monitor.Size, 
-                              Participant.OS, Checkpoint, checkpoint.yiku, Spreadsheet, Spreadsheet.Name, Zone.Name, Reaction.Onset, Response.Type,
-                              X.Coordinate, Y.Coordinate, randomise_blocks, X) )
+PVT = subset(combined_PVT, select = -c(UTC.Timestamp,Local.Timestamp, Local.Timezone, Experiment.ID,Repeat.Key, Schedule.ID, Participant.Public.ID, Participant.Status, Participant.Starting.Group, Participant.Completion.Code,
+                              Participant.External.Session.ID, Participant.Device, UTC.Date, checkpoint.gn26, display, Dishonest, Participant.Private.ID, Tree.Node.Key,
+                               Checkpoint, checkpoint.yiku, Spreadsheet, Spreadsheet.Name, Zone.Name, Reaction.Onset, Response.Type, ï..Event.Index,
+                              X.Coordinate, Y.Coordinate, randomise_blocks, X, Reaction.Time) )
 
 #export combined data as a CSV. 
 write.csv(PVT,"PVT.csv",row.names=FALSE)
